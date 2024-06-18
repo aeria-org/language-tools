b23927a307d7dfff40fd3cfafeab7743a61e40be
ringeringeraja <joaosan177@gmail.com>
2024-06-18 02:10:30 -0300

    ci: verify versions before publishing

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -9,7 +9,21 @@ case $BRANCH_NAME in
   ;;
 esac
 
+ext_name="Aeria.aeria-lang-vscode"
+
+local_version="$(cat packages/vscode/package.json | jq -r '.version')"
+ovsx_version="$(cd packages/vscode; npx ovsx get --metadata "$ext_name" | jq -r '.version')"
+vsce_version="$(cd packages/vscode; npx vsce show --json "$ext_name" | jq -r '.versions[0].version')"
+
 pnpm --filter=./packages/vscode run pack
-pnpm --filter=./packages/vscode publish:ovsx
-pnpm --filter=./packages/vscode publish:vsce
+
+if [ "$local_version" != "$ovsx_version" ]; then
+  echo "Publishing to OpenVSIX because $local_version != $ovsx_version"
+  pnpm --filter=./packages/vscode publish:ovsx
+fi
+
+if [ "$local_version" != "$vsce_version" ]; then
+  echo "Publishing to Visual Studio Marketplace because $local_version != $vsce_version"
+  pnpm --filter=./packages/vscode publish:vsce
+fi
 
