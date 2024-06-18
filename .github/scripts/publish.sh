2f4108cf59157ea781769361115e8a55f7b35bba
ringeringeraja <joaosan177@gmail.com>
2024-06-18 02:13:54 -0300

    ci: fix apt install

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -18,12 +18,16 @@ vsce_version="$(cd packages/vscode; npx vsce show --json "$ext_name" | jq -r '.v
 pnpm --filter=./packages/vscode run pack
 
 if [ "$local_version" != "$ovsx_version" ]; then
-  echo "Publishing to OpenVSIX because $local_version != $ovsx_version"
+  echo "Publishing to OpenVSIX ($local_version != $ovsx_version)"
   pnpm --filter=./packages/vscode publish:ovsx
+else
+  echo "Skipping publish because remote OpenVSIX version matches ($local_version == $ovsx_version)"
 fi
 
 if [ "$local_version" != "$vsce_version" ]; then
-  echo "Publishing to Visual Studio Marketplace because $local_version != $vsce_version"
+  echo "Publishing to VS Marketplace ($local_version != $vsce_version)"
   pnpm --filter=./packages/vscode publish:vsce
+else
+  echo "Skipping publish because remote VS Marketplace version matches ($local_version == $vsce_version)"
 fi
 
