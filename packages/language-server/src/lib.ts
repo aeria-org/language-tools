ebf11550eba9edcf4df3efe8e1c933944b28e181
ringeringeraja <joaosan177@gmail.com>
2025-05-26 12:33:18 -0300

    chore: update LSP

--- packages/language-server/src/lib.ts
+++ packages/language-server/src/lib.ts
@@ -1,17 +1,29 @@
 import type { Connection, TextDocumentChangeEvent } from 'vscode-languageserver'
 import type { TextDocument } from 'vscode-languageserver-textdocument'
 import { fileURLToPath } from 'node:url'
+import { readFile } from 'node:fs/promises'
+import { glob } from 'glob'
 
 export const reportDiagnostics = async ({ document }: TextDocumentChangeEvent<TextDocument>, connection: Connection) => {
   const lang = await import('@aeriajs/compiler')
 
-  const source = document.getText()
-  const result = await lang.parseAndCheck({
-    [fileURLToPath(document.uri)]: source,
-  }, {
+  const currentFileName = fileURLToPath(document.uri).split(process.cwd() + '/').at(-1)
+  const fileList = await glob('**/*.aeria')
+
+  const sources: Record<string, string> = {}
+  for (const fileName of fileList) {
+    if( fileName === currentFileName ) {
+      sources[fileName] = document.getText()
+    } else {
+      sources[fileName] = await readFile(fileName, {
+        encoding: 'utf-8',
+      })
+    }
+  }
+
+  const result = await lang.parseAndCheck(sources, {
     languageServer: true,
   })
-
 
   const diagnostics = result.errors.map((error) => ({
     message: error.message,
